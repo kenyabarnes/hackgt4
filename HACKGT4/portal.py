0a588ec9804ad754ad942e7d6bb73d990a574afc
Kenya Barnes <barneskenya@gmail.com>
2017-10-14 09:31:56 -0400

    Add files via upload

---
+++ HACKGT4/portal.py
@@ -0,0 +1,82 @@
+from twython import Twython
+import time
+import codecs
+
+#Fill these with your application key and application secret, taken from Twitter.
+APP_KEY = 'YOUR_APP_KEY'
+APP_SECRET = 'YOUR_APP_SECRET'
+
+#Use getToken() to get your application-only access token, then replace it here.
+ACCESS_TOKEN = 'YOUR_ACCESS_TOKEN'
+
+#Fill these with your oauth token and token secrets, taken from your Twitter.
+OAUTH_TOKEN = 'YOUR_OAUTH_TOKEN_SECRET'
+OAUTH_TOKEN_SECRET = 'YOUR_OAUTH_TOKEN_SECRET'
+
+def getToken():
+    twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2);
+    print twitter.obtain_access_token()
+
+def getTimeline(screenName, tweetCount = 200, excludeReplies = False):
+    twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
+    results = twitter.get_user_timeline(screen_name=screenName, count=tweetCount)
+    if tweetCount <= 5:
+        for result in results:
+            print 'Tweet ID: ', result['id']
+            print 'Text: ', result['text']
+            print 'Created: ', result['created_at']
+            print '===================//===================\n'
+    else:
+        fp = codecs.open('.\Results\\'+currentTime()+' (TIMELINE).txt', 'w', "utf-8")
+        for result in results:
+            fp.write('Tweet ID: '+str(result['id'])+'\r\n')
+            fp.write('Text: '+result['text']+'\r\n')
+            fp.write('Created: '+str(result['created_at'])+'\r\n')
+            fp.write('===================//===================\r\n')
+
+def getStatus(tweetID):
+        twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
+        results = twitter.show_status(id=tweetID)
+        fp = codecs.open('.\Results\\'+currentTime()+' (STATUS).txt', 'w','utf-8')
+        fp.write('Created: '+results['created_at'])
+        fp.write('\r\n')
+        fp.write(str(results['text']+'\r\n\r\n'))
+        fp.close()
+        
+
+def getFollowers(screenName, userCount = 200, skipStatus = True):
+    twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
+    results = twitter.get_followers_list(screen_name=screenName, count=userCount, skip_status=skipStatus)
+
+    if userCount <= 5:
+        for result in results['users']:
+            print 'ID: ', result['id']
+            print 'Screen Handle: ', result['screen_name']
+            print '===================//==================='
+            print ' '
+    else:
+        fp = codecs.open('.\Results\\'+currentTime()+' (FOLLOWERS).txt', 'w', "utf-8")
+        for result in results['users']:
+            fp.write('ID: '+str(result['id'])+'\r\n')
+            fp.write('Screen Handle: '+result['screen_name']+'\r\n')
+            fp.write('===================//===================\r\n')
+
+def getFollowing(screenName, userCount = 200,skipStatus = True):
+    twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
+    results = twitter.get_friends_list(screen_name=screenName, count=userCount, skip_status=skipStatus)
+    if userCount <= 5:
+        for result in results['users']:
+            print 'ID: ', result['id']
+            print 'Screen Handle: ', result['screen_name']
+            print '===================//==================='
+            print ' '
+    else:
+        fp = codecs.open('.\Results\\'+currentTime()+' (FOLLOWING).txt', 'w', "utf-8")
+        for result in results['users']:
+            fp.write('ID: '+str(result['id'])+'\r\n')
+            fp.write('Screen Handle: '+result['screen_name']+'\r\n')
+            fp.write('===================//===================\r\n')
+
+def currentTime():
+    currentTime = time.strftime('%Y-%m-%d--%H-%M-%S')
+    return currentTime
