fbb08cf3624b2a93b46f03b1db8769c7ccce363a
jdookie67 <jdookie67@gmail.com>
2017-10-15 02:33:00 -0400

    works a bit better

--- HACKGT4/TweetScrapper/src/HACKGT/Scrape.py
+++ HACKGT4/TweetScrapper/src/HACKGT/Scrape.py
@@ -1,25 +1,46 @@
-'''
-Created on Oct 14, 2017
-
-@author: Kenya Barnes
-'''
 from twython import Twython
-import json
+from twython import TwythonStreamer
+import sys
 import ast
 import you_get
 import os
 import subprocess
 
-
 #Application Key and Application Secret from app.twitter.com
 APP_KEY = 'AzOXf1QIO4LUjbi7zpD0RzH6m'
 APP_SECRET = 'tInkuiCumvbsuBcsBje7vi2yfUi4AdQyVKrvm2M429jGcsjcT9'
 
-#
 ACCESS_TOKEN = 'AAAAAAAAAAAAAAAAAAAAANQQ2wAAAAAASOUsj2IFaj%2BlQCOPeJiijU0GH50%3Dw9cdMLGIGpiN82aY2AbfTyjvEOSBu8vLrHF6u093CkQ4n7S73m'
 
 OAUTH_TOKEN = '724142590459826176-kpVZJ6mwP1oN3iMp9e3lccY0Rz8rL3M'
 OAUTH_TOKEN_SECRET = 'BTw0VhVcp7mws7CzDwGSUwyzpApyIsxxz9Wj8toqzgND7'
+class MyStreamer(TwythonStreamer):
+    def on_success(self, data):
+        temp=0
+        if True:
+            if 'retweet_count' in data:
+                temp=data['retweet_count']
+                print (temp)
+                temp= int(temp)
+                print (temp)
+            if 'text' in data :
+                user= data['user']
+                print (data['id_str'])
+                print (user['screen_name'])
+                print (data['contributors'])
+                print (data['retweet_count'])
+                print (data['text'].encode('ascii','ignore'))
+            if 'media' in data:
+                print(data['media'])
+        # Want to disconnect after the first result?
+
+        #self.disconnect()
+        print('\n')
+
+    def on_error(self, status_code, data):
+        print (status_code, data)
+
+
 
 def getToken():
     twitter = Twython(APP_KEY, APP_SECRET,  oauth_version=2)
@@ -38,48 +59,10 @@ def getTimeline(screenName, tweetCount, excludeReplies,files):
 def currentTime():
     currentTime = time.strftime('%Y-%m-%d--%H-%M-%S')
     return currentTime
-
-def ffmpeg_concat_mp4_to_mp4(files, output='output.mp4'):
-    print('Merging video parts... ', end="", flush=True)
-    # Use concat demuxer on FFmpeg >= 1.1
-    if FFMPEG == 'ffmpeg' and (FFMPEG_VERSION[0] >= 2 or (FFMPEG_VERSION[0] == 1 and FFMPEG_VERSION[1] >= 1)):
-        concat_list = generate_concat_list(files, output)
-        params = [FFMPEG] + LOGLEVEL + ['-y', '-f', 'concat', '-safe', '-1',
-                                        '-i', concat_list, '-c', 'copy',
-                                        '-bsf:a', 'aac_adtstoasc', output]
-        subprocess.check_call(params, stdin=STDIN)
-        os.remove(output + '.txt')
-        return True
-
-    for file in files:
-        if os.path.isfile(file):
-            params = [FFMPEG] + LOGLEVEL + ['-y', '-i']
-            params.append(file)
-            params += ['-c', 'copy', '-f', 'mpegts', '-bsf:v', 'h264_mp4toannexb']
-            params.append(file + '.ts')
-
-            subprocess.call(params, stdin=STDIN)
-
-    params = [FFMPEG] + LOGLEVEL + ['-y', '-i']
-    params.append('concat:')
-    for file in files:
-        f = file + '.ts'
-        if os.path.isfile(f):
-            params[-1] += f + '|'
-    if FFMPEG == 'avconv':
-        params += ['-c', 'copy', output]
-    else:
-        params += ['-c', 'copy', '-absf', 'aac_adtstoasc', output]
-
-    subprocess.check_call(params, stdin=STDIN)
-    for file in files:
-        os.remove(file + '.ts')
-    return True
 def dataprocess(status,file):
     ent=status['entities']
     us=status['user']
     temp="lol"
-    #print(status)
     #why try catch you say. im lazy.
     try:
         ent=status['entities']
@@ -104,10 +87,12 @@ def dataprocess(status,file):
             typ2=typ1[0]
         except:
             typ2={'type':'Text'}
-        print(us['screen_name'],status['text'], has2,string['expanded_url'],typ2['type'])
+        string= str(us['screen_name']+" "+status['text']+" "+has2+" "+string['expanded_url']+" "+typ2['type'])+'\n'
+        string.encode(sys.stdout.encoding, errors='replace')
 
-        file.write(str("[{text:" +status['text'] +",intent: None,entities:[{entity: FamilyMember,startPos: 4,endPos: 6}]},"))
-
+        #file.write(str("[{text:" +status['text'] +",intent: None,entities:[{entity: FamilyMember,startPos: 4,endPos: 6}]},"))
+        print(string)
+        file.write(string)
 
     except Exception as e:# should never get here if it does we fucked
         print (e)
@@ -117,26 +102,12 @@ def videoProcess(vidURL):
     if(not(vidURL is 'N/A')):
         os.system("you-get "+vidURL)
     return
-output= open("out.txt",'w')
-getTimeline('@realDonaldTrump', 20, False,output)
+#steam
+stream = MyStreamer(APP_KEY, APP_SECRET,OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+#stream.statuses.filter(track=' twitter')
+
+
+output= open("out.txt",'w', encoding='utf-8')
+getTimeline('@realDonaldTrump', 5, True,output)
 #getStatus()
 #status= getStatus(918960024256434176)#(vid) 918960024256434176 (pic) 918894502185787392 (text) 919009334016856065
-#print(status)
-#dataprocess(status,output)
-#videoProcess(string['expanded_url'])
-#os.system("you-get https://m.twitter.com/realDonaldTrump/status/918960024256434176/video/1")
-
-
-'''cmd='you-get -n' + string['expanded_url']
-#cmd='you-get http://www.fsf.org/blogs/rms/20140407-geneva-tedx-talk-free-software-free-society'
-proc='hi'
-#proc = subprocess.run(["you-get","-n" ,string['expanded_url']], stdout=subprocess.PIPE)
-os.system(cmd)
-#print(proc)
-#temp =ffmpeg_concat_mp4_to_mp4(proc,"output")
-command = "cmd.exe"
-stdin=''
-stdout=''
-proc = subprocess.Popen(command, stdin = subprocess.PIPE, stdout = subprocess.PIPE)
-proc.stdin.write(cmd)
-print(stdout)'''
